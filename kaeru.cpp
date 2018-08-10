b973bd3828a729a7a2c12bba4e4927300987dd23
Steinar H. Gunderson <sgunderson@bigfoot.com>
2018-08-10 17:47:32 +0200

    Unbreak Kaeru.

--- kaeru.cpp
+++ kaeru.cpp
@@ -47,9 +47,9 @@ int write_packet(void *opaque, uint8_t *buf, int buf_size, AVIODataMarkerType ty
 
 	if (type == AVIO_DATA_MARKER_HEADER) {
 		stream_mux_header.append((char *)buf, buf_size);
-		httpd->set_header(HTTPD::MAIN_STREAM, stream_mux_header);
+		httpd->set_header(stream_mux_header);
 	} else {
-		httpd->add_data(HTTPD::MAIN_STREAM, (char *)buf, buf_size, type == AVIO_DATA_MARKER_SYNC_POINT, time, AVRational{ AV_TIME_BASE, 1 });
+		httpd->add_data((char *)buf, buf_size, type == AVIO_DATA_MARKER_SYNC_POINT, time, AVRational{ AV_TIME_BASE, 1 });
 	}
 	return buf_size;
 }
